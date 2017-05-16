6075070d8ee419cd1eaeeb51a3ac1567f4152181
Gregory Santone Jr <gregory.santone@uconn.edu>
2017-05-16 14:22:05 -0400

    Still trying to fix dir

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Copying files to remote server..."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR cti-admin@137.99.8.3
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/travis_ci_testing cti-admin@137.99.8.3:travis_ci_testing
 # git remote add deploy ssh://cti-admin@137.99.8.3/srv/git/travis_ci_testing.git
 # git push deploy master
 

da8a7a676e0825b5ab69965f1bc45bf5a06bd1d4
Gregory Santone Jr <gregory.santone@uconn.edu>
2017-05-16 15:30:59 -0400

    Try starting server without sudo

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ cti-admin@137.99.8.3:travis_c
 echo "Done copying!"
 
 echo "Starting server..."
-ssh cti-admin@137.99.8.3 "sudo start travis_ci_testing"
+ssh cti-admin@137.99.8.3 "start travis_ci_testing"
 echo "Server started!"

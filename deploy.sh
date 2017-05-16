82db164a9592383b32ccf3ae7a7b8430193db7dc
Gregory Santone Jr <gregory.santone@uconn.edu>
2017-05-16 14:18:59 -0400

    Prevent nested dir

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Copying files to remote server..."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR cti-admin@137.99.8.3:travis_ci_testing
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR cti-admin@137.99.8.3
 # git remote add deploy ssh://cti-admin@137.99.8.3/srv/git/travis_ci_testing.git
 # git push deploy master
 
